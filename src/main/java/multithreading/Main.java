246cf1611e1c5b906b27ccfcd8cbc9dd8ffd6652
kelevv <kelevich.v@gmail.com>
2025-06-16 10:01:10 +0300

    ping pong

--- src/main/java/multithreading/Main.java
+++ src/main/java/multithreading/Main.java
@@ -2,6 +2,22 @@ package multithreading;
 
 public class Main {
     public static void main(String[] args) {
-        int i;
+        Thread t1 = new Thread(new MyRunnable("PING"));
+        Thread t2 = new Thread(new MyRunnable("PONG"));
+
+        System.out.println("Game start");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+
+        System.out.println("Game over");
+
     }
 }
