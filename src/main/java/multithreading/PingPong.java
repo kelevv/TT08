a53a72a8281335ae0f2041208c450ac57dca5fc6
kelevv <kelevich.v@gmail.com>
2025-06-16 10:04:14 +0300

    PING PONG!

---
+++ src/main/java/multithreading/PingPong.java
@@ -0,0 +1,21 @@
+package multithreading;
+
+public class PingPong {
+    public static void run() {
+        Thread t1 = new Thread(new MyRunnable("PING"));
+        Thread t2 = new Thread(new MyRunnable("PONG"));
+        System.out.println("GAME START!");
+
+        t1.start();
+        t2.start();
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            System.out.println("Main thread was interrupted");
+        }
+
+        System.out.println("GAME OVER!");
+    }
+}
